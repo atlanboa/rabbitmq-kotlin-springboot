6991ffc96e468535fbec826f2887ce97d3fb02d6
Lunit <lunit@Lunitui-MacBookPro-4.local>
2021-06-07 13:13:12 +0900

    how to configure rabbitmq and multi-module

---
+++ consumer/src/test/kotlin/scope/lunit/rabbitmq/RabbitmqApplicationTests.kt
@@ -0,0 +1,13 @@
+package scope.lunit.rabbitmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "rabbitmq"
+include("producer")
+include("consumer")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
